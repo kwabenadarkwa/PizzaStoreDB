2d0f61128f0fce41da4cd9256a7f627a248393fe
kwabenadarkwa <darkwak@live.com>
2024-10-28 10:19:17 +0000

    :sparkles:adds the microsoft entity framework

--- Program.cs
+++ Program.cs
@@ -1,6 +1,30 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.OpenApi.Models;
 
-app.MapGet("/", () => "Hello World!");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(static c =>
+{
+    c.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Title = "PizzaStore API",
+            Description = "Making the Pizzas you love",
+            Version = "v1",
+        }
+    );
+});
+WebApplication app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI(static c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+    });
+}
+
+app.MapGet("/", static () => "Hello World!");
 
 app.Run();
